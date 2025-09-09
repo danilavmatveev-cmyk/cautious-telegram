ca1de741d012e746a6a1fdb8af9899714fa3074d
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-09-09 20:11:01 +0300

    Update multiplication_table.py

--- multiplication_table.py
+++ multiplication_table.py
@@ -2,7 +2,7 @@ number1 = input("Введите первое число: ")
 symbol = input("Введите символ операции: ")
 number2 = input("Введите второе число: ")
 while True:
-    if number1 
+    if number1
     if abs(number1).isdigit() and abs(number2).isdigit():
     number1 = float(number1)
     number2 = float(number2)
