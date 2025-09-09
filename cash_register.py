05a74e1d48fca0984942fe4b06cc0e0493140e10
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-09-09 16:09:20 +0300

    Update cash_register.py

--- cash_register.py
+++ cash_register.py
@@ -1,5 +1,18 @@
 quantity = input("Введите количество товаров: ")
 while True:
-    if tiles.isdigit():
-    for i in range(quantity+1):
-    price = input("Введите цену товара: ")
+    if quantity.isdigit():
+        quantity = int(quantity)
+        total = 0
+        for i in range(quantity):
+            price = input(f"Введите цену товара {i+1}: ")
+            if price.isdigit():
+                price = int(price)
+                total  += price
+            else:
+                print("Ощибка: Введите число для цены!")
+        print(f"Общая сумма: {total}")
+        break
+    else:
+        print("Ошибка: введите число для количества!")
+        quantity = input("Введите количество товаров: ")
+    
