d323daef19f7efad99e3790947bffd76b2317db1
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-09-09 07:26:11 +0300

    Update multiplication_table.py

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,9 +1,9 @@
 number1 = input("Введите первое число: ")
 symbol = input("Введите символ операции: ")
 number2 = input("Введите второе число: ")
-n1 = abs(number1)
-n2 = abs(number2)
-if n1.isdigit() and n2.isdigit():
+while True:
+    if number1 
+    if abs(number1).isdigit() and abs(number2).isdigit():
     number1 = float(number1)
     number2 = float(number2)
     if symbol == "*":
