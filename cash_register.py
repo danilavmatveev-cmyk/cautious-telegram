002ba75e295c13ceaa363ad44d9cf1e3c8c2f7b8
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-09-10 12:04:08 +0300

    Update cash_register.py

--- cash_register.py
+++ cash_register.py
@@ -8,10 +8,36 @@ while True:
             if price.isdigit():
                 price = int(price)
                 total  += price
+                break
             else:
-                print("Ощибка: Введите число для цены!")
+                print("Ошибка: введите число для количества!")
+
         print(f"Общая сумма: {total}")
+
+        if total >= 500:
+            discount = 0
+            discount_text = "нет"
+            total_amount = total
+        elif total % 13 == 0 or total % 101 == 0:
+            discount = 31
+            discount_text = "31 %"
+            total_amount = total * 0.69
+        else:
+            discount = 5
+            discount_text = "5%"
+            total_amount = total * 0.95
+
+        print(f"Скидка: {discount_text}")
+        print(f"Итоговая сумма: {total_amount:.2f}")
+        print(f"Количество товаров: {quantity}")
+        average_price = total_amount / quantity
+        print(f"Средняя цена: {average_price:.2f}")
+
         break
     else:
         print("Ошибка: введите число для количества!")
         quantity = input("Введите количество товаров: ")
+
+
+
+
