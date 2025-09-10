de5be9ddad79ab3b9bf8215bdbfd30ecfbc37106
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-09-10 15:18:02 +0300

    Create Guess_number.py

---
+++ Guess_number.py
@@ -0,0 +1,31 @@
+import random
+random_number = random.randint(0, 100)
+def bin_search(random_number):
+    number = int(input(" "))
+    while number != random_number:
+        if number < random_number:
+            print(f"Неверно! Число больше {number}")
+        elif number > random_number:
+            print(f"Неверно! Число меньше {number}")
+
+        try:
+            number = int(input(" "))
+        except ValueError:
+            print("Пожалуйста, введите целое число!")
+            continue
+
+    print("Верно!")
+
+
+
+bin_search(100)
+
+
+
+
+
+
+
+
+
+
