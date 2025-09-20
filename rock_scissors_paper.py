4f01679795111ce46132ddc33e109b1db7db6309
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-09-20 13:04:40 +0300

    Update rock_scissors_paper.py

--- rock_scissors_paper.py
+++ rock_scissors_paper.py
@@ -1,49 +1,26 @@
 import random
-seed =['камень','ножницы',"бумага"]
-random_seed = random.choice(seed)
-student_main  = input("Сделайте свой ход (камень\ножницы\бумага): ")
-i = 0
-j = 0
-while i <= 7 or j <= 7:
+
+seed = ["камень","ножницы","бумага"]
+
+computer_score = 0
+student_score = 0
+while computer_score < 7 and student_score < 7:
+    random_seed = random.choice(seed)
+    student_main = input("Сделайте свой ход (камень\ножницы\бумага): ").lower().strip()
+
+    if student_main not in seed:
+        print("Пожалуйста, введите одно из трех: камень, ножницы или бумага")
+        continue
     if student_main == random_seed:
-        i = i+0
-        j = j+0
-        print(f"компьютер - ученик \n{random_seed} - {student_main}  \n{i} - {j}, (ничья)")
+        print(f"компьютер - ученик \n{random_seed} - {student_main}  \n{computer_score} - {student_score} (ничья)")
+    elif(student_main == "камень" and random_seed == "ножницы") or \
+        (student_main == "ножницы" and random_seed == "бумага") or \
+        (student_main == "бумага" and random_seed == "камень"):
+        student_score += 1
+        print(f"компьютер - ученик \n{random_seed} - {student_main}  \n{computer_score} - {student_score}  (выиграл ученик)")
     else:
-        for random_seed in seed[0]:
-            if student_main == "ножницы":
-                i = i+1
-                j = j+0
-                print(f"компьютер - ученик \n{random_seed} - {student_main}  \n{i} - {j},  (выиграл компьютер)")
-            elif student_main == "бумага":
-                i = i+0
-                j = j+1
-                print(f"компьютер - ученик \n{random_seed} - {student_main}  \n{i} - {j},  (выиграл ученик)")
-            else:
-                print("Введите правильное слово!")
+        computer_score += 1
+        print(f"компьютер - ученик \n{random_seed} - {student_main}  \n{computer_score} - {student_score}  (выиграл компьютер)")
 
-        for random_seed in seed[1]:
-            if student_main == "бумага":
-                i = i+1
-                j = j+0
-                print(f"компьютер - ученик \n{random_seed} - {student_main}  \n{i} - {j},  (выиграл компьютер)")
-            elif student_main == "камень":
-                i = i+0
-                j = j+1
-                print(f"компьютер - ученик \n{random_seed} - {student_main}  \n{i} - {j},  (выиграл ученик)")
-            else:
-                print("Введите правильное слово!")
-
-        for random_seed in seed[2]:
-            if student_main == "камень":
-                i = i + 1
-                j = j + 0
-                print(f"компьютер - ученик \n{random_seed} - {student_main}  \n{i} - {j},  (выиграл компьютер)")
-            elif student_main == "ножницы":
-                i = i + 0
-                j = j + 1
-                print(f"компьютер - ученик \n{random_seed} - {student_main}  \n{i} - {j},  (выиграл ученик)")
-            else:
-                print("Введите правильное слово!")
-
-
+    if computer_score == 7 and student_score == 7:
+        print("Конец игры!")
