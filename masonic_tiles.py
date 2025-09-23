d44ce64795944271e873f0712a5f0595e7c92940
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-09-23 21:59:22 +0300

    Update masonic_tiles.py

--- masonic_tiles.py
+++ masonic_tiles.py
@@ -2,14 +2,20 @@ while True:
     tiles = input("Введите количество плиток ")
     if tiles.isdigit():
         tiles = int(tiles)
-        if tiles == 0:
-            print(" ")
+        if tiles % 2 == 0:
+            if tiles == 0:
+                print(" ")
+            else:
+                for i in range(tiles):
+                    for j in range(tiles):
+                        if (i+j) % 2 == 0:
+                            print("■", end=" ")
+                        else:
+                            print("□", end=" ")
+                    print()
         else:
-            for i in range(tiles):
-                for j in range(tiles):
-                    print("■ □ ", end="")
-                print()
+            print("Ошибка,нужно ввести четное число!")
     else:
-        print("Ошибка, нужно ввести целое число!")
-        continue
+       print("Ошибка, нужно ввести целое число!")
+       continue
     break
