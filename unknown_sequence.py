8635298075772cd97d2571ced6fea41e773f460c
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-09-23 10:23:43 +0300

    Create unknown_sequence.py

---
+++ unknown_sequence.py
@@ -0,0 +1,20 @@
+n1 = input("ведите первое число: ")
+n2 = input("введите второе число: ")
+N = input ("введите количество чисел в последовательности: ")
+a = 0
+try:
+   n1 = int(n1)
+   n2 = int(n2)
+   N = int(N)
+
+   seq_list = [n1,n2]
+   for i in range(N-2):
+     a = n1 + n2 + n1*n2
+     seq_list.append(a)
+     n1 = n2
+     n2 = a
+     i += 1
+   print(seq_list)
+
+except ValueError:
+    print("Ошибка! Введите корректное число!")
