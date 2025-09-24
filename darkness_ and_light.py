53b21b1bbe81369d56679fef3954449ba992bdcb
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-09-24 11:37:32 +0300

    Update darkness_ and_light.py

--- darkness_ and_light.py
+++ darkness_ and_light.py
@@ -1,2 +1,18 @@
+letter = ["С","Т"]
+seq = []
+import random
+for i in range(5):
+    random_letter = random.choice(letter)
+    seq.append(random_letter)
 def check(seq: str) -> str:
-    
+    student = input("Введите энергию (Свет/Тьма): ").lower().strip()
+    res = seq.count ("С")
+    if (res >= 3 and student == "свет") or (res < 3 and student == "тьма"):
+        print('Падаван достоин звания "Рыцаря-джедая"!')
+    else:
+        print("Падаван остаётся на дополнительное обучение...")
+check(seq)
+
+
+
+
