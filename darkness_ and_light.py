d6412e76a2c68f91f25c7bb511c6d5267e7f69f9
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-09-24 12:31:50 +0300

    Update darkness_ and_light.py

--- darkness_ and_light.py
+++ darkness_ and_light.py
@@ -4,14 +4,22 @@ import random
 for i in range(5):
     random_letter = random.choice(letter)
     seq.append(random_letter)
+    result_seq = "".join(seq)
 def check(seq: str) -> str:
-    student = input("Введите энергию (Свет/Тьма): ").lower().strip()
-    res = seq.count ("С")
-    if (res >= 3 and student == "свет") or (res < 3 and student == "тьма"):
-        print('Падаван достоин звания "Рыцаря-джедая"!')
-    else:
-        print("Падаван остаётся на дополнительное обучение...")
-check(seq)
+    while True:
+        student = input("Введите энергию (Свет/Тьма): ").lower().strip()
+        if student == "свет" or student == "тьма":
+            res = seq.count("С")
+            if (res >= 3 and student == "свет") or (res < 3 and student == "тьма"):
+                print(f'Последовательность из 5 кристаллов: {result_seq}\nПадаван достоин звания "Рыцаря-джедая"!')
+            else:
+                print(f"Последовательность из 5 кристаллов: {result_seq}\nПадаван остаётся на дополнительное обучение...")
+            break
+        else:
+            print("Ошибка! Введите либо свет, либо тьма!")
+            
+
+check(result_seq)
 
 
 
