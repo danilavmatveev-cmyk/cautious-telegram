eaca1848a23e98b056b73ff6938e6a7f0aa5b682
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-09-28 21:53:53 +0300

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,6 @@
 number = input("Введите число: ")
 number_str = number.replace(' ', '')
-reversed_number_str1 = number_str[::-1]
+reversed_number_str = number_str[::-1]
 
 try:
     number_float = float(number_str)
