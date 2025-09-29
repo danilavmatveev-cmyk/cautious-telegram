74f7478e93ea2a1588d7aed5026ffa7ee9ddcc06
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-09-29 16:57:39 +0300

    Create suitcase_hacking.py

---
+++ suitcase_hacking.py
@@ -0,0 +1,23 @@
+def guess_code(password: str):
+    pasword_list = list(password)
+    for n in range 5:
+        code = input("")
+        code_list = list[code]
+        number = 0
+        if pasword_list[n] == code_list[n]
+            
+
+
+
+        if
+        for i in range code:
+
+
+        if number < random_number:
+            print(f"Неверно! Число больше {number}")
+        elif number > random_number:
+            print(f"Неверно! Число меньше {number}")
+        else:
+            print("Верно!")
+            break
+
