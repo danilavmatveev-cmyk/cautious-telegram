af4ee2eec2baea285cdacb6e47bc6d1dbaa69417
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-09-30 12:15:16 +0300

    Update suitcase_hacking.py

--- suitcase_hacking.py
+++ suitcase_hacking.py
@@ -1,23 +1,37 @@
-def guess_code(password: str):
-    pasword_list = list(password)
-    for n in range 5:
+def guess_code(pasword: str):
+
+    pasword_list = list(pasword)
+
+
+    for j in range(9):
         code = input("")
-        code_list = list[code]
+        code_list = list(code)
         number = 0
-        if pasword_list[n] == code_list[n]
-            
+        place_number = 0
+        if len(code_list) == 5:
+            if code_list != pasword_list:
+                for i in range(len(pasword_list)):
+                    if code_list[i] in pasword_list:
+                       number = number + 1
+                    else:
+                        number = number + 0
 
 
+                for n in range(len(code_list)):
+                    if pasword_list[n] == code_list[n]:
+                        place_number = place_number + 1
+                    else:
+                        place_number = place_number + 0
 
-        if
-        for i in range code:
-
-
-        if number < random_number:
-            print(f"Неверно! Число больше {number}")
-        elif number > random_number:
-            print(f"Неверно! Число меньше {number}")
+                print(f"Угадано цифр: {number}")
+                print(f"На своих местах: {place_number}")
+                print("Неверно, попробуйте ещё раз")
+            else:
+                print(f"Поздравляем! Вы угадали пароль {pasword} за {j} попыток!")
+                break
         else:
-            print("Верно!")
-            break
+            print("Ошибка! Нужно ввести ровно 5 цифр")
+
+guess_code("39412")
+
 
