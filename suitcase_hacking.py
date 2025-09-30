3e1b2414012a4cedaf2dae5c7f00c9d64ff91575
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-09-30 16:26:37 +0300

    Update suitcase_hacking.py

--- suitcase_hacking.py
+++ suitcase_hacking.py
@@ -12,31 +12,33 @@ def guess_code(password: str):
 
         attempts += 1
 
-        if len(code_list) == 5:
-            number = 0
-            place_number = 0
-            if code_list == password_list:
-                print(f"Угадано цифр: 5")
-                print(f"На своих местах: 5" + "\n")
-                print(f"Поздравляем! Вы угадали пароль {password} за {attempts} попыток!")
-                break
+
+
+
+        if code_list == password_list:
+            print(f"Угадано цифр: 5")
+            print(f"На своих местах: 5" + "\n")
+            print(f"Поздравляем! Вы угадали пароль {password} за {attempts} попыток!")
+            break
+
+        number = 0
+        for i in range(len(password_list)):
+            if code_list[i] in password_list:
+                number = number + 1
             else:
-                for i in range(len(password_list)):
-                    if code_list[i] in password_list:
-                        number = number + 1
-                    else:
-                        number = number + 0
+                number = number + 0
 
-                for n in range(len(code_list)):
-                    if password_list[n] == code_list[n]:
-                        place_number = place_number + 1
-                    else:
-                        place_number = place_number + 0
+        place_number = 0
+        for n in range(len(code_list)):
+            if password_list[n] == code_list[n]:
+                place_number = place_number + 1
+            else:
+                place_number = place_number + 0
 
 
-                print(f"Угадано цифр: {number}")
-                print(f"На своих местах: {place_number}" + "\n")
-                print("Неверно, попробуйте ещё раз")
+        print(f"Угадано цифр: {number}")
+        print(f"На своих местах: {place_number}" + "\n")
+        print("Неверно, попробуйте ещё раз")
 
 
 
