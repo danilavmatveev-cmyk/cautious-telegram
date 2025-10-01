279cf7ee3e66962cf71be3d8ec05006d132ef4a1
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-10-01 20:55:53 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,29 +1,21 @@
-
+import sys
 number_list = []
 operator_list = []
 symbol = ""
 while symbol != "=":
-    try:
+    number = float(input("Число: "))
+    number_list.append(number)
 
-        number = float(input("Введите число: "))
-        number_list.append(number)
-
-        symbol = input("Введите оператор (+,-,*,/,^) или =: ")
-
-        if symbol == "=":
-            break
-        elif symbol in ["+", "-", "*", "/","^"]:
-            operator_list.append(symbol)
-        else:
-            print("Ошибка! Неверный оператор. Попробуйте снова.")
-            continue
-
-    except ValueError:
-        print("Ошибка! Введите корректное число.")
+    symbol = input("Оператор (+,-,*,/,^) или =: ")
+    if symbol == "=":
+        break
+    else:
+        operator_list.append(symbol)
         continue
 
-result = number_list[0]
+result = number_list [0]
 for i in range(len(operator_list)):
+
     next_number = number_list[i + 1]
 
 
@@ -36,13 +28,13 @@ for i in range(len(operator_list)):
     elif operator_list[i] == '/':
         if next_number == 0:
             print("Ошибка: деление на ноль!")
-            result = "не определено"
-            break
+            sys.exit()
         result /= next_number
-    elif operator_list[i] == '^':
+
+    else:
         result **= next_number
 
-print(f"Результат: {result}")
+print(result)
 
 
 
