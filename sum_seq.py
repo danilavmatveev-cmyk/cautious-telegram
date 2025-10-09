4036345038cda1b250c424cff6d42a0a9a6e5071
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-10-09 09:18:58 +0300

    Create sum_seq.py

---
+++ sum_seq.py
@@ -0,0 +1,13 @@
+numbers = int(input())
+sequence_list = []
+for i in range (numbers):
+    sequence  = int(input())
+    sequence_list.append(sequence)
+    print(sequence_list)
+sum_sequence = 0
+for j in range(len(sequence_list)):
+    if sequence_list [j] % 6 == 0 and sequence_list [j] % 10 == 4:
+        sum_sequence += 1
+    else:
+        sum_sequence += 0
+print(sum_sequence)
