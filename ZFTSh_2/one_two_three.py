403b5dbec2bfffac35c0ea7658a10e17967d1e6e
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-10-11 12:44:17 +0300

    fdfd

---
+++ ZFTSh_2/one_two_three.py
@@ -0,0 +1,29 @@
+cache = {}
+
+
+
+def one_two_three(current, target):
+
+    if (current, target) in cache:
+        return cache[(current, target)]
+
+    if current == target:
+        return 1
+    if current > target:
+        return 0
+    if current == 33:
+        return 0
+
+
+
+
+    cache[(current, target)] = (one_two_three(current + 1, target) +
+              one_two_three(current * 2, target) +
+              one_two_three(current * 3, target))
+
+
+    return cache[(current, target)]
+
+total = one_two_three(2,15) * one_two_three(15,50)
+
+print(total)
