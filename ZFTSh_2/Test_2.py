c76638ff25aaa2e6ac0cc3deaf6b22e5d26caa38
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-10-13 16:58:52 +0300

    Update Test_2.py

--- ZFTSh_2/Test_2.py
+++ ZFTSh_2/Test_2.py
@@ -3,10 +3,16 @@ seq_list = []
 while numbers != 0:
     seq_list.append(numbers)
     numbers = int(input())
-new_list = []
-for i in range(len(seq_list)):
 
-    if seq_list[i] % 3 == 0:
-        new_list.append(seq_list[i])
+def seq_div_three(seq_list, cache = {}):
+    new_list = []
 
-print(sum(new_list))
+    for i in range(len(seq_list)):
+        if seq_list[i] in cache:
+            return cache[i]
+        if seq_list[i] % 3 == 0:
+            new_list.append(seq_list[i])
+
+    print(sum(new_list))
+
+seq_div_three(seq_list)
