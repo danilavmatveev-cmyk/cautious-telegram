f8aa790f4d922bdb1726b964dfe3f6ef951c750a
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-10-18 09:46:20 +0300

    Update can_king_move.py

--- ZFTSh_2/can_king_move.py
+++ ZFTSh_2/can_king_move.py
@@ -1,10 +1,10 @@
 def can_king_move(x1, y1, x2, y2):
-    if x1 == x2 and  (y2 == y1 +1 or y1 == y1 - 1):
+    if x1 == x2 and  (y2 == y1 +1 or y2 == y1 - 1):
         return True
-    if x2 == x1-1 and (y1 == y1 or y2 == y1+1 or y1 == y1 - 1):
+    if (x2 == x1-1 or x2 == x1+1) and (y2 == y1 or y2 == y1+1 or y2 == y1 - 1):
         return True
-    if x2 == x1+1 and (y1 == y1 or y2 == y1+1 or y1 == y1 - 1):
+    if  y1 == y2 and (x2 == x1 + 1 or x2 == x1 - 1):
         return True
-    else:
-        return False
+
+    return False
 print(can_king_move(1,1,2,3))
