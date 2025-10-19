31b0f9c469c3d1a112d18566d41dafc66130adc6
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-10-19 18:05:25 +0300

    Create hanoi_towers.py

---
+++ ZFTSh_2/hanoi_towers.py
@@ -0,0 +1,20 @@
+def hanoi_towers(n, from_rod, to_rod, aux_rod):
+
+    if n == 0:
+        return  ""
+
+    if n == 1:
+        result = f"Переместить диск {n} со стержня {from_rod} на стержень {to_rod}\n"
+        return result
+
+    step1 = hanoi_towers(n - 1, from_rod, aux_rod, to_rod)
+    step2 = f"Переместить диск {n} со стержня {from_rod} на стержень {to_rod}\n"
+    step3 = hanoi_towers(n - 1, aux_rod, to_rod, from_rod)
+    result = step1+step2+step3
+    return result
+
+
+
+
+
+print(hanoi_towers(3, "A", "C", "B"))
