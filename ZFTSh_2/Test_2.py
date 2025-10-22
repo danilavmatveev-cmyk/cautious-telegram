6c1123cbb90e1e54ce94e0678e0a92597ad5526c
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-10-22 09:49:31 +0300

    Update Test_2.py

--- ZFTSh_2/Test_2.py
+++ ZFTSh_2/Test_2.py
@@ -4,11 +4,11 @@ def grasshopper_ways (n):
     if n == 1:
         return 1
     dp = [0]*(n+1)
-
+    dp[0] = 1
     dp[1] = 1
-    dp[2] = 2
-    for i in range(3, n+1):
+
+    for i in range(2, n+1):
         dp[i] =dp[i-1]+dp[i-2]
     return dp[n]
-print(grasshopper_ways (11))
+print(grasshopper_ways (1))
 
