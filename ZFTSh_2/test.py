b75330c45f0c4acb03476d75ad70e621d47cd1cc
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-10-23 12:44:31 +0300

    Update test.py

--- ZFTSh_2/test.py
+++ ZFTSh_2/test.py
@@ -1,26 +1,41 @@
 def grasshopper_with_obstacles(n, obstacles):
 
     if not obstacles:
-        if n == 0:
+        if n == 0: # прыжок на месте
             return 1
         if n == 1:
             return 1
         dp = [0] * (n + 1)
         dp[0] = 1
         dp[1] = 1
-        for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i-3]
+        dp[2] = 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
         return dp[n]
+
+
     else:
-        if n == 0:
-            return 1
-        if n == 1:
-            return 0
         dp = [0] * (n + 1)
-        dp[0] = 1
-        dp[1] = 0
-        for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
+        if n == 0: # прыжок на месте
+            dp[0] = 1
+            return dp[n]
+        if n == 1 and obstacles == 1:
+            dp[1] = 1
+            return dp[n]
+        if n == 2 and (obstacles == [1] or [2]):
+            dp[2] = 1
+            return dp[n]
+        if n == 2 and obstacles == [1,2]:
+            dp[2] = 0
+            return dp[n]
+        if n == 3 and obstacles == [1,2]:
+            dp[3] = 1
+            return dp[n]
+
+
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2] +dp[i-3]
         return dp[n]
 
 
@@ -28,6 +43,6 @@ def grasshopper_with_obstacles(n, obstacles):
 
 
 
-print(grasshopper_with_obstacles(4,[3]))
+print(grasshopper_with_obstacles(5,[2]))
 
 
