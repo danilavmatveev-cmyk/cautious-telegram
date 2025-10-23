97f95b14041b86b4e9149cef5e8fd7e6ea6e8799
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-10-23 17:13:20 +0300

    ffg

--- ZFTSh_2/Test_2.py
+++ ZFTSh_2/Test_2.py
@@ -1,14 +1,50 @@
-def grasshopper_ways (n):
-    if n == 0:
-        return 1
-    if n == 1:
-        return 1
-    dp = [0]*(n+1)
-    dp[0] = 1
-    dp[1] = 1
+def grasshopper_with_obstacles(n, obstacles):
+    if not obstacles:
+        if n == 0:  # прыжок на месте
+            return 1
+        if n == 1:
+            return 1
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        if n >= 2:
+            dp[2] = 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+        return dp[n]
+    else:
+        dp = [0] * (n + 1)
 
-    for i in range(2, n+1):
-        dp[i] =dp[i-1]+dp[i-2]
-    return dp[n]
-print(grasshopper_ways (1))
+        # Инициализация для позиции 0
+        if 0 < len(obstacles) and obstacles[0] == 1:
+            dp[0] = 0
+        else:
+            dp[0] = 1
 
+        # Инициализация для позиции 1
+        if n >= 1:
+            if 1 < len(obstacles) and obstacles[1] == 1:
+                dp[1] = 0
+            else:
+                # На позицию 1 можно попасть только с позиции 0
+                dp[1] = dp[0]
+
+        # Инициализация для позиции 2
+        if n >= 2:
+            if 2 < len(obstacles) and obstacles[2] == 1:
+                dp[2] = 0
+            else:
+                # На позицию 2 можно попасть с позиций 0 и 1
+                dp[2] = dp[0] + dp[1]
+
+        # Заполнение для остальных позиций
+        for i in range(3, n + 1):
+            if i < len(obstacles) and obstacles[i] == 1:
+                dp[i] = 0
+            else:
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+        return dp[n]
+
+
+print(grasshopper_with_obstacles(2, [2]))
--- ZFTSh_2/test.py
+++ ZFTSh_2/test.py
@@ -1,48 +1,43 @@
 def grasshopper_with_obstacles(n, obstacles):
-
-    if not obstacles:
-        if n == 0: # прыжок на месте
-            return 1
-        if n == 1:
-            return 1
-        dp = [0] * (n + 1)
-        dp[0] = 1
-        dp[1] = 1
-        dp[2] = 2
-        for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
-        return dp[n]
+    if n < 0:
+        return 0
 
 
+    blocked_obs = set(obstacles)
+
+    #
+    dp = [0] * (n + 1)
+
+
+    if 0 in blocked_obs:
+        return 0
     else:
-        dp = [0] * (n + 1)
-        if n == 0: # прыжок на месте
-            dp[0] = 1
-            return dp[n]
-        if n == 1 and obstacles == 1:
-            dp[1] = 1
-            return dp[n]
-        if n == 2 and (obstacles == [1] or [2]):
-            dp[2] = 1
-            return dp[n]
-        if n == 2 and obstacles == [1,2]:
-            dp[2] = 0
-            return dp[n]
-        if n == 3 and obstacles == [1,2]:
-            dp[3] = 1
-            return dp[n]
+        dp[0] = 1
 
+    # Заполняем массив dp для клеток от 1 до n
+    for i in range(1, n + 1):
+        if i in blocked_obs:
+            dp[i] = 0
+        else:
+            # Прыжок на +1
+            if i - 1 >= 0:
+                dp[i] += dp[i - 1]
 
+            # Прыжок на +2
+            if i - 2 >= 0:
+                dp[i] += dp[i - 2]
 
-        for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2] +dp[i-3]
-        return dp[n]
+            # Прыжок на +3
+            if i - 3 >= 0:
+                dp[i] += dp[i - 3]
 
+    return dp[n]
 
 
 
 
 
-print(grasshopper_with_obstacles(5,[2]))
+
+print(grasshopper_with_obstacles(3,[2]))
 
 
