55913c3e28dd3377d04f705e5a62cba1bb58d73b
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-10-24 08:45:05 +0300

    Create grasshopper_with_obstacles.py

---
+++ ZFTSh_2/grasshopper_with_obstacles.py
@@ -0,0 +1,33 @@
+def grasshopper_with_obstacles(n, obstacles):
+    if n < 0:
+        return 0
+
+
+    blocked_points = set(obstacles)
+    dp = [0] * (n + 1)
+
+
+    if 0 in blocked_points:
+        return 0
+    else:
+        dp[0] = 1
+
+    for i in range(1, n + 1):
+        if i in blocked_points:
+            dp[i] = 0
+        else:
+            if i - 1 >= 0:
+                dp[i] = dp[i] + dp[i - 1]
+            if i - 2 >= 0:
+                dp[i] = dp[i] + dp[i - 2]
+            if i - 3 >= 0:
+                dp[i] = dp[i] + dp[i - 3]
+
+    return dp[n]
+
+
+
+
+
+
+print(grasshopper_with_obstacles(3,[2]))
