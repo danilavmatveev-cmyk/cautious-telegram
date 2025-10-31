c4e93a4a24767a128b24057e2521a54e7ad834bd
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-10-31 14:21:37 +0300

    Update prime_number.py

--- ZFTSh_2/prime_number.py
+++ ZFTSh_2/prime_number.py
@@ -1,14 +1,17 @@
 def is_prime(n):
 
-    if n == 1:
-            return False
+    if n <= 1:
+        return False
     if n == 2:
-            return True
-    for i in range(2,n):
+        return True
+    if n % 2 == 0:
+        return False
+
+    for i in range(3,int(n**0.5+1),2):
         if n % i == 0:
             return False
 
     return True
 
 
-print(is_prime(3))
+print(is_prime(37))
