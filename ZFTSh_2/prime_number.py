c7f92165be0ff370c9cd9e84ca30291359b8f1bb
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-10-31 12:17:50 +0300

    Update prime_number.py

--- ZFTSh_2/prime_number.py
+++ ZFTSh_2/prime_number.py
@@ -1,19 +1,14 @@
-def is_prime(n,cache = {}):
-    if n in cache:
-        return cache[n]
+def is_prime(n):
+
     if n == 1:
-        cache[n] = False
-        return False
+            return False
     if n == 2:
-        cache[n] = True
-        return True
+            return True
     for i in range(2,n):
         if n % i == 0:
-            cache[n] = False
             return False
 
-    cache[n]= True
     return True
 
 
-print(is_prime(910))
+print(is_prime(3))
