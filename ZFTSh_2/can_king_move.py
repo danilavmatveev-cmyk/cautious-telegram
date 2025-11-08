b9e9c373f741097922cb69dc3e890c6b6875ca88
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-11-08 09:30:02 +0300

    пп

--- ZFTSh_2/can_king_move.py
+++ ZFTSh_2/can_king_move.py
@@ -1,5 +1,5 @@
 def can_king_move(x1, y1, x2, y2):
-    if x1 == x2 and  (y2 == y1 +1 or y2 == y1 - 1):
+    if x1 == x2 and  (y2 == y1 + 1 or y2 == y1 - 1):
         return True
     if (x2 == x1-1 or x2 == x1+1) and (y2 == y1 or y2 == y1+1 or y2 == y1 - 1):
         return True
