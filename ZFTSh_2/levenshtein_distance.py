16d48023f56aed6131110e70b36375dd1c97c9ae
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-11-17 11:59:50 +0300

    Update levenshtein_distance.py

--- ZFTSh_2/levenshtein_distance.py
+++ ZFTSh_2/levenshtein_distance.py
@@ -1,17 +1,20 @@
 def levenshtein_distance(s1, s2):
     if s1==s2:
         return 0
-    dp = [[0]*(s1+1) for _ in range(len(s2)+1)]
 
-    dp[0][0] = 0
-  j от 1 до N
-   D(0, j) = D(0, j - 1) + цена вставки символа S2[j]
- для всех i от 1 до M
-   D(i, 0) = D(i - 1, 0) + цена удаления символа S1[i]
-   для всех j от 1 до N
-     D(i, j) = min{
-       D(i - 1, j) + цена удаления символа S1[i],
-       D(i, j - 1) + цена вставки символа S2[j],
-       D(i - 1, j - 1) + цена замены символа S1[i] на символ S2[j]
-     }
- вернуть D(M, N)
+    dp = [[0]*(len(s2)+1) for _ in range(len(s1)+1)]
+
+    for i in range(1, len(s1)+1):
+        dp[i][0] = i
+    for j in range (1, len(s2)+1):
+       dp[0][j] = j
+
+    for i in range(1, len(s1) + 1):
+        for j in range(1, len(s2) + 1):
+            if s1[i-1] == s2[j-1]:
+                cost = 0
+            else:
+                cost = 1
+            dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + cost)
+    return dp[len(s1)][len(s2)]
+print(levenshtein_distance("kitten", "sitting"))
