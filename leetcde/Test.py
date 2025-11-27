25a132229d78dac49a7e55e7f1979500ce2e7119
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-11-27 16:32:52 +0300

    Update Test.py

--- leetcde/Test.py
+++ leetcde/Test.py
@@ -1,15 +1,20 @@
 class Solution:
-    def smallestRepunitDivByK(self, k: int) -> int:
-        if k % 2 == 0 or k % 5 == 0:
-            return -1
-        remainder = 1
-        length = 1
-        while remainder % k != 0:
-            remainder = (remainder * 10 + 1) % k
-            length += 1
-            if length > k:
-                return -1
-        return length
+    def maxSubarraySum(self, nums: List[int], k: int) -> int:
+        def quick_sort(arr):
+            if len(arr) <= 1:
+                return arr
+            pivot = arr[len(arr) // 2]
+            left = [x for x in arr if x < pivot]
+            middle = [x for x in arr if x == pivot]
+            right = [x for x in arr if x > pivot]
+            return quick_sort(left) + middle + quick_sort(right)
+        if k == 1 or  k == len(nums):
+            return sum(nums)
+        n = len(nums) % k
+        new_nums = quick_sort(nums)
+        return sum(new_nums[n:])
+
+
 
 
 
