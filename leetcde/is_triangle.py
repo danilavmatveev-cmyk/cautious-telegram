b563479b1d06717b2226fdd32336baa0ed6a05ee
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-12-09 10:22:14 +0300

    fghfg

--- leetcde/is_triangle.py
+++ leetcde/is_triangle.py
@@ -1,21 +1,21 @@
-def is_triangle(a,b,c):
-    if a+b<=c or a+c<=b or b+c<=a:
+def is_triangle(a, b, c):
+    if a + b <= c or a + c <= b or b + c <= a:
         return False
     else:
         return True
+
+
 def input_is_triangle():
     try:
-        a = int(input ("Введите a:" ))
-        b = int(input ("Введите b:" ))
-        c = int(input ("Введите c:" ))
-
+        a = int(input("Введите a:"))
+        b = int(input("Введите b:"))
+        c = int(input("Введите c:"))
 
         result = is_triangle(a, b, c)
         print(result)
     except ValueError:
         print("Ошибка: пожалуйста, введите целые числа")
 
+
 if __name__ == "__main__":
     input_is_triangle()
-
-
