2cba16c0c11554898843f7c9a3afd40e8a261342
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-12-10 17:17:35 +0300

    Update test2.py

--- leetcde/test2.py
+++ leetcde/test2.py
@@ -1,1 +1,8 @@
-def fignya
+import math
+def hypotenuse(a,b):
+    csquared = a ** 2 + b ** 2
+    print('сумма квадратов равна: ', csquared)
+    result = math.sqrt(csquared)
+    print(f'Длина гипоттнузы равна: {result:.2f}')
+    return result
+hypotenuse(2,4)
