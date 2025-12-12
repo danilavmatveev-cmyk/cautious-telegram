5d15789e1e918d67dcaf55cc6ac30208347ed226
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-12-12 18:41:33 +0300

    Update is_power.py

--- leetcde/is_power.py
+++ leetcde/is_power.py
@@ -1,10 +1,10 @@
 def is_power(a,b):
 
-    if a/b == 1:
+    if a == 1:
         return True
-    elif a%b ==0:
-        return is_power(a/b,b)
-    else:
+    elif a % b != 0 or a < b:
         return False
+    else:
+        return is_power(a // b, b)
 
 print (is_power(12,2))
