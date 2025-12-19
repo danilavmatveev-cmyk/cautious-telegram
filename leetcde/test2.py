87f88af5cb632a249b97ced30d25f20be925307c
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2025-12-19 17:15:03 +0300

    Update test2.py

--- leetcde/test2.py
+++ leetcde/test2.py
@@ -1,8 +1,15 @@
 def has_no_e(word):
-    for letter in word:
+    for letter in word.lower:
         if letter == 'e':
             return False
     return True
+
+if has_no_e():
+    with open('words.txt', 'r') as fin:
+        for line in fin:
+            word = line.strip()
+            if 'e' not in word.lower():
+                print(word)
 
 
 
