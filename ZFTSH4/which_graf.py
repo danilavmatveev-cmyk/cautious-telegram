8f52281deaca95e03668d44728439c2f01a1d066
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2026-01-22 17:42:13 +0300

    Update which_graf.py

--- ZFTSH4/which_graf.py
+++ ZFTSH4/which_graf.py
@@ -2,6 +2,8 @@ N = int(input())
 G = [[0] * N for i in range(N)]
 for i in range(N):
     G[i] = list(map(int, input().split()))
+
+
 def exam_graph(G):
     N = len(G)
     mask_orien = True
@@ -11,5 +13,19 @@ def exam_graph(G):
                 mask_orien = False
                 break
     return mask_orien
+
+
+def count__degree(G):
+    N = len(G)
+    count = 0
+
+    for i in range(N):
+
+        degree = sum(G[i]) + G[i][i]
+
+        if degree % 2 == 0:
+            count += 1
+
+    return count
 result = exam_graph(G)
 print(result)
