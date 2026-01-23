d3523c86a65ef6cc96c2779fc9d93a0fadd7620f
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2026-01-23 17:19:40 +0300

    о

---
+++ ZFTSH4/adjacency_matrix_of_graph.py
@@ -0,0 +1,7 @@
+V_list = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+V,E = map(int,input().split())
+lists = [[] for i in range(V)]
+
+for _ i in range(E):
+    neighbors = input().split()
+    adj_list.append(neighbors)
