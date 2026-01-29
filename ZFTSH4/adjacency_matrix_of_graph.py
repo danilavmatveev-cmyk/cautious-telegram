f84c8e5260b9fea863d3a89ef68b837756a9a433
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2026-01-29 16:10:52 +0300

    Update adjacency_matrix_of_graph.py

--- ZFTSH4/adjacency_matrix_of_graph.py
+++ ZFTSH4/adjacency_matrix_of_graph.py
@@ -1,7 +1,19 @@
-V_list = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-V,E = map(int,input().split())
-lists = [[] for i in range(V)]
 
-for _ i in range(E):
-    neighbors = input().split()
-    adj_list.append(neighbors)
+
+def input_graf():
+    N = int(input())
+    G = [[0]*N for i in range(N)]
+    vertex_index = {V_list[i]: i for i in range(N)}
+    for i in range(N):
+       
+        stok = (list(input().split()))
+        for v in stok:
+            G[i][vertex_index[v]]=1
+            G[vertex_index[v]][i]=1
+    return N,G
+
+V_list = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+N,G = input_graf()
+for i in range(N):
+    print(*G[i])
+
