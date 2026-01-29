1da9a6d14fc5dfbe8845c55c4e54e0e24e57ebec
danilavmatveev-cmyk <danilavmatveev@gmail.com>
2026-01-29 16:40:46 +0300

    Update adjacency_matrix_of_graph.py

--- ZFTSH4/adjacency_matrix_of_graph.py
+++ ZFTSH4/adjacency_matrix_of_graph.py
@@ -5,8 +5,7 @@ def input_graf():
     G = [[0]*N for i in range(N)]
     vertex_index = {V_list[i]: i for i in range(N)}
     for i in range(N):
-       
-        stok = (list(input().split()))
+        stok = input().split()
         for v in stok:
             G[i][vertex_index[v]]=1
             G[vertex_index[v]][i]=1
